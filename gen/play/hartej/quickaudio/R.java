701c7046cc3c656b27d75ce930168ec1b09ef121
Ethan Hart <ethan.john.hart@gmail.com>
2014-01-17 15:24:04 -0500

    Fix start/stop audio; Update icon

--- gen/play/hartej/quickaudio/R.java
+++ gen/play/hartej/quickaudio/R.java
@@ -22,9 +22,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int superman=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int action_settings=0x7f090004;
+        public static final int button1=0x7f090001;
+        public static final int button2=0x7f090002;
+        public static final int button3=0x7f090003;
+        public static final int textView1=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,7 +43,10 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060002;
+        public static final int start=0x7f060004;
+        public static final int stop=0x7f060005;
+        public static final int superman=0x7f060002;
+        public static final int web_page=0x7f060003;
     }
     public static final class style {
         /** 
--- src/play/hartej/quickaudio/MainActivity.java
+++ src/play/hartej/quickaudio/MainActivity.java
@@ -1,26 +1,69 @@
 package play.hartej.quickaudio;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+	MediaPlayer superman;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	Log.e("Pickle", "at onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MediaPlayer superman = MediaPlayer.create(this, R.raw.superman);
-        superman.start();	
+        //superman.start();	
+        superman = MediaPlayer.create(this, R.raw.superman);
     }
+    
+    public void startAudio(View v) {
+    	Log.e("Pickle", "at startAudio");
+    	if(superman.isPlaying()){
+    		Log.e("Player", "Audio is playing");
+    	}else{
+    		superman.start();
+    	}
+    }
+    
+    public void stopAudio(View v) {
+    	Log.e("Pickle", "at stopAudio");
+    	if(superman.isPlaying()) {
+    		superman.stop();
+    		superman.prepareAsync();
+    	}
+    	//superman.reset();
+    }
+    
+    
+    //@Override
+    //protected void onResume() {
+    	//superman.start();
+    	//super.onResume();
+    //}
 
+    //@Override
+    //protected void onPause() {
+    	//superman.stop();
+    	//super.onPause();
+    //}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void openDC(View v) {
+    	String url = "http://www.dccomics.com/characters/superman";
+    	Intent i = new Intent(Intent.ACTION_VIEW);
+    	i.setData(Uri.parse(url));
+    	startActivity(i);
     }
     
 }
